12a6035c42e7ccf9af13edd142d3d793c7acea55
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2021-11-07 21:04:50 +0200

    feat: continue reading a book

--- src/app.ts
+++ src/app.ts
@@ -1539,24 +1539,39 @@ import {type} from "os"
 // enum E {F, G}
 // let e: E = E.F
 
-type State = {
-  [key: string]: string // we can add a lot properties with string key like 'text'. It is equal to string like 'text'
-}
+// type State = {
+//   [key: string]: string // we can add a lot properties with string key like 'text'. It is equal to string like 'text'
+// }
+// 
+// class StringDatabase {
+//   constructor(public state: State = {}) {}
+//   get(key: string): string | null {
+//     return key in this.state ? this.state[key] : null
+//   }
+//   set(key: string, value: string): void {
+//     this.state[key] = value
+//   }
+//   static from(state: State) {
+//     let db = new StringDatabase
+//     for (let key in state) {
+//       db.set(key, state[key])
+//     }
+//     return db
+//   }
+// }
+// 
+// interface StringDatabase {
+//   state: State
+//   get(key: string): string | null
+//   set(key: string, value: string): void
+// }
+// 
+// interface StringDatabaseConstructor {
+//   new(state?: State): StringDatabase
+//   from(state: State): StringDatabase
+// }
 
-class StringDatabase {
-  state: State = {}
-  get(key: string): string | null {
-    return key in this.state ? this.state[key] : null
-  }
-  set(key: string, value: string): void {
-    this.state[key] = value
-  }
-  static from(state: State) {
-    let db = new StringDatabase
-    for (let key in state) {
-      db.set(key, state[key])
-    }
-    return db
-  }
-}
+// Currently can not debug StringDatabaseConstructor, StringDatabase, State
+
+// 8
 
