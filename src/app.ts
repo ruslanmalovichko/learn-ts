31ffad3da1cf8b3e1e681a9087e56c121f06b03c
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2021-11-28 22:38:26 +0200

    feat: continue reading a book

--- src/app.ts
+++ src/app.ts
@@ -1697,3 +1697,32 @@ import {type} from "os"
 // new MessageQueue([]) // error, constructor is private
 // MessageQueue.create([]) // we can use create method
 
+// 14
+type Shoe = {
+  purpose: string
+}
+
+class BalletFlat implements Shoe {
+  purpose = 'dancing'
+}
+
+class Boot implements Shoe {
+  purpose = 'woodcutting'
+}
+
+class Sneaker implements Shoe {
+  purpose = 'walking'
+}
+
+let Shoe = {
+  create(type: 'balletFlat' | 'boot' | 'sneaker'): Shoe {
+    switch (type) {
+      case 'balletFlat': return new BalletFlat
+      case 'boot': return new Boot
+      case 'sneaker': return new Sneaker
+    }
+  }
+}
+
+console.log(Shoe.create('boot')) // Creates object of Boot class
+
